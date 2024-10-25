81370ba9ce57658f583a8ce3b713a2f0e2bf411e
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-10-25 15:09:29 +0530

    Demonstrating arrays

---
+++ arrays/code1.js
@@ -0,0 +1,14 @@
+//Demonstrating arrays 
+
+let arr = [91, 87,23,67,98,87,"Not Present"]; // array can consists of multiple types of data
+console.log(arr[0]); //accessing array elements using index
+console.log(arr[1]); 
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+arr[7] = 100; //we can also add elements to the array
+arr[0] = 35; //also arrays are mutable we can change an existing element in a array
+
+console.log(arr);
