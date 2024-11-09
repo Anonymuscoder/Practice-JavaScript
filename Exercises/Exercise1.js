836f88f171b11b04375d76f1e66ee310b32b5616
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-11-09 16:36:41 +0530

    Modified Code: Added Scoring Feature to track the score of the player and edited the alerts

--- Exercises/Exercise1.js
+++ Exercises/Exercise1.js
@@ -2,15 +2,16 @@
 
 let randomNumber = Math.floor(Math.random()*101);
 console.log(randomNumber);
-alert("Guess Game: We will generate a number, you have to Guess that number, and you have 10 chances to Guess the Number")
+alert("Guess Game: We will generate a number, you have to Guess that number, and you have 10 chances to Guess the Number, If you guess the number in the first place you will get maximum score which is 100, for each wrong guess 10 points will be deducted from the maximum score")
 let guess;
+let score = 100;
 let difference;
 let chances = 10;
 do{
     guess = prompt("Enter your Guess");
     guess = Number.parseInt(guess);
     if(guess == randomNumber){
-        alert("You Guessed Right");
+        alert("You Guessed Right, Actual Number is "+randomNumber+", Your score is "+score);
         break;
     }
     else{
@@ -19,11 +20,13 @@ do{
             let result = Math.abs(difference);
             chances--;
             alert("Guessed Wrong! Actual Number is greater than the Guessed Number. You are now remained with only "+chances+" chances, Try Again...");
+            score = score - 10;
         }
         else{
             let result = difference;
             chances--;
             alert("Guessed Wrong! Actual Number is lesser than the Guessed Number. You are now remained with only "+chances+" chances, Try Again...");
+            score = score - 10;
         }
     }
 }while(chances>0);
