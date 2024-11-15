18ed1a955da4177e9b716f66c0d6aa7259ec452b
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-11-15 15:50:15 +0530

    Chances count added after every loss, user will get alert with displaying number of  chances remained

--- Exercises/Exercise2.js
+++ Exercises/Exercise2.js
@@ -9,28 +9,34 @@ let input = () => {
   }
 };
 
+let count = 10;
+
 let result = (str) => {
   const arr = ["Snake", "Water", "Gun"];
   let rp = arr[Math.floor(Math.random() * arr.length)];
   console.log(rp);
   console.log(rp + "v/s" + str);
+  count--;
   if (rp == str) {
-    alert("Draw!, Try Again..");
+    alert("Draw!, Try Again.. You have "+count+" chances remained");
     input();
   } else if (rp == "Gun" && str == "Snake") {
-    alert("You lose!!, Try Again..");
+    alert("You lose!!, Try Again.. You have "+count+" chances remained");
     input();
   } else if (rp == "Water" && str == "Gun") {
-    alert("You lose!!, Try Again..");
+    alert("You lose!!, Try Again.. You have "+count+" chances remained");
     input();
   } else if (rp == "Snake" && str == "Water") {
-    alert("You lose!!, Try Again..");
+    alert("You lose!!, Try Again.. You have "+count+" chances remained");
     input();
-  } else {
+  } else if(count==0){
+    alert("Out of Chances!!.. You lose");
+  }else {
     alert("You Win!!");
-
+    
     let replay = confirm("Want to play again??");
     if (replay) {
+      count = 10;
       input();
     } else {
     }
